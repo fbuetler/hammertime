c8fc0e5c2a2dd0d970999d646e31076d3c6f7ed2
Florian Buetler <florian.buetler@hotmail.com>
2023-03-08 15:57:01 +0100

    basic map with tiles

--- src/hammered/GameMain.cs
+++ src/hammered/GameMain.cs
@@ -1,41 +1,80 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace hammered;
 
 public class GameMain : Game
 {
     private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+
+    private BasicEffect _basicEffect;
+
+    private Matrix _worldMatrix, _viewMatrix, _projectionMatrix;
+
+    Model rubiksCubeModel;
+    float blockSize = 10f;
+    int xBlocks = 15;
+    int zBlocks = 10;
 
     public GameMain()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
-        IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        // setup our graphics scene matrices
+        float xMapCenter = xBlocks / 2 * blockSize;
+        float zMapCenter = zBlocks / 2 * blockSize;
 
-        // TODO: use this.Content to load your game content here
+        Vector3 cameraPosition = new Vector3(xMapCenter, 100f, zMapCenter + 75f);
+        Vector3 cameraTarget = new Vector3(xMapCenter, 0f, zMapCenter); // look at middle of the tiles
+
+        float fovAngle = MathHelper.ToRadians(45); // field of view
+        float aspectRatio = (float)_graphics.PreferredBackBufferWidth / _graphics.PreferredBackBufferHeight;
+        float near = 0.01f;
+        float far = 1000f;
+
+        _worldMatrix = Matrix.Identity;
+        _viewMatrix = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Up);
+        _projectionMatrix = Matrix.CreatePerspectiveFieldOfView(fovAngle, aspectRatio, near, far);
+
+        // Setup our basic effect
+        _basicEffect = new BasicEffect(GraphicsDevice);
+        _basicEffect.World = _worldMatrix;
+        _basicEffect.View = _viewMatrix;
+        _basicEffect.Projection = _projectionMatrix;
+        _basicEffect.VertexColorEnabled = true;
+
+        rubiksCubeModel = Content.Load<Model>("RubiksCube");
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        KeyboardState currentKeys = Keyboard.GetState();
 
-        // TODO: Add your update logic here
+        //Press Esc To Exit
+        if (currentKeys.IsKeyDown(Keys.Escape))
+            this.Exit();
+
+
+        //Press Directional Keys to rotate cube
+        if (currentKeys.IsKeyDown(Keys.Up))
+            _worldMatrix *= Matrix.CreateRotationX(-0.05f);
+        if (currentKeys.IsKeyDown(Keys.Down))
+            _worldMatrix *= Matrix.CreateRotationX(0.05f);
+        if (currentKeys.IsKeyDown(Keys.Left))
+            _worldMatrix *= Matrix.CreateRotationY(-0.05f);
+        if (currentKeys.IsKeyDown(Keys.Right))
+            _worldMatrix *= Matrix.CreateRotationY(0.05f);
 
         base.Update(gameTime);
     }
@@ -44,8 +83,37 @@ public class GameMain : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        for (int i = 0; i < xBlocks; i++)
+        {
+            for (int j = 0; j < zBlocks; j++)
+            {
+                DrawModel(rubiksCubeModel, i, j);
+            }
+        }
 
         base.Draw(gameTime);
+    }
+
+    private void DrawModel(Model model, int i, int j)
+    {
+        Matrix translation = Matrix.CreateTranslation(i * blockSize, 0f, j * blockSize);
+
+        foreach (ModelMesh mesh in rubiksCubeModel.Meshes)
+        {
+            foreach (BasicEffect effect in mesh.Effects)
+            {
+                //effect.EnableDefaultLighting();
+                effect.AmbientLightColor = new Vector3(1f, 0, 0);
+                effect.World = _worldMatrix;
+
+                // translate tiles
+                Matrix translatedView = new Matrix();
+                Matrix.Multiply(ref translation, ref _viewMatrix, out translatedView);
+                effect.View = translatedView;
+
+                effect.Projection = _projectionMatrix;
+            }
+            mesh.Draw();
+        }
     }
 }
