9dee372128efdef16ad5f6e1efd264208496e932
Florian Buetler <flbuetle@ethz.ch>
2023-04-11 01:49:09 +0200

    create own menu with images

--- src/hammered/Game/HudOverlay.cs
+++ src/hammered/Game/HudOverlay.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using hammered;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
--- src/hammered/Game/Menu.cs
+++ src/hammered/Game/Menu.cs
@@ -1,125 +1,256 @@
 using System;
-using Apos.Gui;
-using FontStashSharp;
+using System.Collections.Generic;
+using Apos.Input;
 using hammered;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.Extended.TextureAtlases;
+using Microsoft.Xna.Framework.Input;
 
 enum MenuState
 {
-    Main,
-    Settings,
-    Quit
+    TITLE,
+
+    MAIN_START,
+    MAIN_SETTINGS,
+    MAIN_QUIT,
+
+    PLAYERS_2,
+    PLAYERS_3,
+    PLAYERS_4,
+
+    SETTINGS, // TODO (fbuetler) subcategories
+
+    QUIT_YES,
+    QUIT_NO,
 };
 
 public class Menu : DrawableGameComponent
 {
+    public GameMain GameMain { get => _game; }
     private GameMain _game;
-    private IMGUI _ui;
-    private MenuState _state = MenuState.Main;
 
-    private float _slider = 50f;
+    private MenuState _state = MenuState.TITLE;
 
-    private TextureRegion2D _texture;
+    private Dictionary<MenuState, string> _menuPaths;
+
+    public Dictionary<string, Texture2D> Menus { get => _menus; }
+    private Dictionary<string, Texture2D> _menus;
+
+    public static int PlayerIndex = 0;
+
+    public static ICondition Start { get; set; } =
+        new AnyCondition(
+            new KeyboardCondition(Keys.Space),
+            new KeyboardCondition(Keys.Enter),
+            new GamePadCondition(GamePadButton.A, PlayerIndex),
+            new GamePadCondition(GamePadButton.Start, PlayerIndex)
+        );
+
+    public static ICondition Back { get; set; } =
+        new AnyCondition(
+            new KeyboardCondition(Keys.Escape),
+            new GamePadCondition(GamePadButton.B, PlayerIndex),
+            new GamePadCondition(GamePadButton.Back, PlayerIndex)
+        );
+
+    public static ICondition Interact { get; set; } =
+        new AnyCondition(
+            new KeyboardCondition(Keys.Space),
+            new KeyboardCondition(Keys.Enter),
+            new GamePadCondition(GamePadButton.A, PlayerIndex)
+        );
+
+    public static ICondition FocusPrev { get; set; } =
+        new AnyCondition(
+            new KeyboardCondition(Keys.Up),
+            new AllCondition(
+                new AnyCondition(
+                    new KeyboardCondition(Keys.LeftShift),
+                    new KeyboardCondition(Keys.RightShift)
+                ),
+                new KeyboardCondition(Keys.Tab)
+            ),
+            new GamePadCondition(GamePadButton.Up, PlayerIndex)
+        );
+
+    public static ICondition FocusNext { get; set; } =
+        new AnyCondition(
+            new KeyboardCondition(Keys.Down),
+            new KeyboardCondition(Keys.Tab),
+            new GamePadCondition(GamePadButton.Down, PlayerIndex)
+        );
+
+    public static ICondition MoveLeft { get; set; } =
+    new AnyCondition(
+        new KeyboardCondition(Keys.Left),
+        new GamePadCondition(GamePadButton.Left, PlayerIndex)
+    );
+
+    public static ICondition MoveRight { get; set; } =
+        new AnyCondition(
+            new KeyboardCondition(Keys.Right),
+            new GamePadCondition(GamePadButton.Right, PlayerIndex)
+        );
 
     public Menu(Game game) : base(game)
     {
         _game = (GameMain)game;
+
+        _menuPaths = new Dictionary<MenuState, string>();
+        _menuPaths[MenuState.TITLE] = "Menu/title";
+        _menuPaths[MenuState.MAIN_START] = "Menu/Main/main_start";
+        _menuPaths[MenuState.MAIN_SETTINGS] = "Menu/Main/main_settings";
+        _menuPaths[MenuState.MAIN_QUIT] = "Menu/Main/main_quit";
+        _menuPaths[MenuState.PLAYERS_2] = "Menu/Players/players_2";
+        _menuPaths[MenuState.PLAYERS_3] = "Menu/Players/players_3";
+        _menuPaths[MenuState.PLAYERS_4] = "Menu/Players/players_4";
+        _menuPaths[MenuState.SETTINGS] = "Menu/Settings/settings";
+        _menuPaths[MenuState.QUIT_YES] = "Menu/Quit/quit_yes";
+        _menuPaths[MenuState.QUIT_NO] = "Menu/Quit/quit_no";
+
+        _menus = new Dictionary<string, Texture2D>();
     }
 
     protected override void LoadContent()
     {
-        FontSystem fontSystem = FontSystemFactory.Create(GraphicsDevice, 2048, 2048);
-        fontSystem.AddFont(TitleContainer.OpenStream($"{_game.Content.RootDirectory}/Fonts/Arial.ttf"));
-
-        GuiHelper.Setup(_game, fontSystem);
-        _ui = new IMGUI();
-        GuiHelper.CurrentIMGUI = _ui;
-
-        var texture = _game.Content.Load<Texture2D>("Menu/icecream");
-        _texture = new TextureRegion2D(texture, 0, 0, texture.Width, texture.Height);
+        foreach (MenuState state in Enum.GetValues(typeof(MenuState)))
+        {
+            if (!Menus.ContainsKey(state.ToString()))
+            {
+                var texture = GameMain.Content.Load<Texture2D>(_menuPaths[state]);
+                Menus.Add(state.ToString(), texture);
+            }
+        }
     }
 
     public override void Update(GameTime gameTime)
     {
-        GuiHelper.UpdateSetup(gameTime);
-        _ui.UpdateAll(gameTime);
-
-        MenuPanel.Push();
         switch (_state)
         {
-            case MenuState.Main:
-                SetupMainMenu(gameTime);
+            // title
+            case MenuState.TITLE:
+                if (Start.Pressed())
+                    _state = MenuState.MAIN_START;
                 break;
-            case MenuState.Settings:
-                SetupSettingsMenu(gameTime);
+
+            // main
+            case MenuState.MAIN_START:
+                if (FocusPrev.Pressed())
+                    _state = MenuState.MAIN_QUIT;
+                else if (FocusNext.Pressed())
+                    _state = MenuState.MAIN_SETTINGS;
+                else if (Interact.Pressed())
+                    _state = MenuState.PLAYERS_2;
+                break;
+            case MenuState.MAIN_SETTINGS:
+                if (FocusPrev.Pressed())
+                    _state = MenuState.MAIN_START;
+                else if (FocusNext.Pressed())
+                    _state = MenuState.MAIN_QUIT;
+                else if (Interact.Pressed())
+                    _state = MenuState.SETTINGS;
+                break;
+            case MenuState.MAIN_QUIT:
+                if (FocusPrev.Pressed())
+                    _state = MenuState.MAIN_SETTINGS;
+                else if (FocusNext.Pressed())
+                    _state = MenuState.MAIN_START;
+                else if (Interact.Pressed())
+                    _state = MenuState.QUIT_YES;
+                break;
+
+            // players
+            case MenuState.PLAYERS_2:
+                if (FocusPrev.Pressed())
+                    _state = MenuState.PLAYERS_4;
+                else if (FocusNext.Pressed())
+                    _state = MenuState.PLAYERS_3;
+                else if (Interact.Pressed())
+                {
+                    _state = MenuState.MAIN_START;
+                    StartMap(2);
+                }
+                break;
+            case MenuState.PLAYERS_3:
+                if (FocusPrev.Pressed())
+                    _state = MenuState.PLAYERS_2;
+                else if (FocusNext.Pressed())
+                    _state = MenuState.PLAYERS_4;
+                else if (Interact.Pressed())
+                {
+                    _state = MenuState.MAIN_START;
+                    StartMap(3);
+                }
+                break;
+            case MenuState.PLAYERS_4:
+                if (FocusPrev.Pressed())
+                    _state = MenuState.PLAYERS_3;
+                else if (FocusNext.Pressed())
+                    _state = MenuState.PLAYERS_2;
+                else if (Interact.Pressed())
+                {
+                    _state = MenuState.MAIN_START;
+                    StartMap(4);
+                }
+                break;
+
+            // settings
+            case MenuState.SETTINGS:
+                break;
+
+            // quit
+            case MenuState.QUIT_YES:
+                if (FocusPrev.Pressed() || FocusNext.Pressed())
+                    _state = MenuState.QUIT_NO;
+                else if (Interact.Pressed())
+                    GameMain.Exit();
+                break;
+            case MenuState.QUIT_NO:
+                if (FocusPrev.Pressed() || FocusNext.Pressed())
+                    _state = MenuState.QUIT_YES;
+                else if (Interact.Pressed())
+                    _state = MenuState.MAIN_START;
                 break;
             default:
-                SetupQuitMenu(gameTime);
-                break;
+                throw new ArgumentOutOfRangeException(nameof(_state), $"Unexpected menu state: {_state}");
         }
-        MenuPanel.Pop();
 
-        if (Default.Back.Pressed())
+        if (Back.Pressed())
         {
-            if (_state == MenuState.Main)
+            if (_state == MenuState.MAIN_START)
             {
-                _state = MenuState.Quit;
+                _state = MenuState.MAIN_QUIT;
             }
             else
             {
-                _state = MenuState.Main;
+                _state = MenuState.MAIN_START;
             }
         }
 
-        GuiHelper.UpdateCleanup();
         base.Update(gameTime);
     }
 
-    private void SetupMainMenu(GameTime gameTime)
+    private void StartMap(int NumberOfPlayers)
     {
-        Button settingsButton = Button.Put("Settings");
-        if (settingsButton.Clicked)
-            _state = MenuState.Settings;
 
-        Button quitButton = Button.Put("Quit");
-        if (quitButton.Clicked)
-            _state = MenuState.Quit;
-    }
-
-    private void SetupSettingsMenu(GameTime gameTime)
-    {
-        Label.Put("Settings");
-
-        Slider slider = Slider.Put(ref _slider, 0f, 100f, 1f);
-        Label.Put($"{Math.Round(_slider, 1)}");
-
-        Icon.Put(_texture);
-
-        Button backButton = Button.Put("Back");
-        if (backButton.Clicked)
-            _state = MenuState.Main;
-    }
-
-    private void SetupQuitMenu(GameTime gameTime)
-    {
-        Label.Put("Quit Menu");
-
-        Button yesButton = Button.Put("Yes");
-        if (yesButton.Clicked)
-            _game.Exit();
-
-        Button noButton = Button.Put("No");
-        if (noButton.Clicked)
-            _state = MenuState.Main;
     }
 
     public override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.Black);
 
-        _ui.Draw(gameTime);
+        // _spriteBatch.Begin alters the state of the graphics pipeline
+        // therefore we have to reenable the depth buffer here
+        _game.SpriteBatch.Begin(depthStencilState: DepthStencilState.Default);
+
+        var texture = Menus[_state.ToString()];
+        Vector2 scale = new Vector2(
+            GameMain.GetBackBufferWidth() / (float)texture.Width,
+            GameMain.GetBackBufferHeight() / (float)texture.Height
+        );
+        GameMain.SpriteBatch.Draw(texture, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+        _game.SpriteBatch.End();
 
         base.Draw(gameTime);
     }
